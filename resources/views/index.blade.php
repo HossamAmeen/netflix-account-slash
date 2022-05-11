1effe962b9c3f13970aafbb9d84f2f6f35495733
hossam <hossamameen948@gmail.com>
2022-05-11 22:32:17 +0200

    add landing page

---
+++ resources/views/index.blade.php
@@ -0,0 +1,85 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="stylesheet" href="./assets/css/all.min.css">
+  <link rel="stylesheet" href="./assets/css/bootstrap.min.css">
+  <link rel="stylesheet" href="./assets/css/aos.css">
+  <link rel="stylesheet" href="./assets/css/style.css">
+  <title>Document</title>
+</head>
+
+<body>
+
+  <!-- navbar -->
+  <div class="mynavbar">
+    <div class="container">
+      <div class="wrapper">
+        <div class="logo">
+          <a href=""><img src="./assets/images/logo.svg" alt=""></a>
+        </div>
+        <div class="links">
+          <ul class="list-unstyled">
+            <li class="active"><a href="{{url('/')}}">Home</a></li>
+            <li><a href="https://t.me/marvelixgarans" target="_blank">Telegram / Support</a></li>
+          </ul>
+        </div>
+      </div>
+    </div>
+  </div>
+
+  <!-- header -->
+  <div class="header" style="background-image: url('./assets/images/header.png');">
+    <div class="container">
+      <div class="header_content">
+        <h1>Welcome to KlaimGa<span>ransi&nbsp;</span></h1>
+        <h5>Auto Account Replacement</h5>
+        <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the
+          industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and
+          scrambled it to make a type specimen book.
+        </p>
+        <div class="links">
+          <button class="btn btn_style" data-bs-toggle="modal" data-bs-target="#premiumBlanModal">Premium Plan</button>
+          <a href="https://t.me/marvelixgarans" target="_blank" class="btn btn_style">Contact Us</a>
+        </div>
+      </div>
+    </div>
+  </div>
+
+
+
+  <!-- Modal -->
+
+  <div class="modal fade" id="premiumBlanModal" tabindex="-1" aria-labelledby="premiumBlanModalLabel"
+    aria-hidden="true">
+    <div class="modal-dialog modal-dialog-centered">
+      <div class="modal-content"> 
+        <a class="closemodal" data-bs-dismiss="modal" aria-label="Close"><i class="fas fa-times"></i></a>
+        <div class="modal-body">
+          <h5>Starter</h5>
+          <h6>Premium Plan </h6>
+          <h1>RP.199.000</h1>
+          <h4>Per account / 1 year</h6>
+          <p>Advanced features for your
+            business to grow
+          </p>
+          <a href="https://nonton.orderonline.id/ntflx" target="_blank" class="btn btn_style">Buy now!</a>
+        </div>
+      </div>
+    </div>
+  </div>
+
+
+
+
+  <script src="./assets/js/jquery-3.3.1.min.js"></script>
+  <script src="./assets/js/popper.min.js"></script>
+  <script src="./assets/js/bootstrap.min.js"></script>
+  <script src="./assets/js/aos.js"></script>
+  <script src="./assets/js/main.js"></script>
+</body>
+
+</html>
